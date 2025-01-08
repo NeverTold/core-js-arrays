24b11ab93bd9b220305e8f2ca76a6aa384371204
NeverTold <149240019+NeverTold@users.noreply.github.com>
2025-01-09 02:53:16 +0700

    fix: intervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -21,7 +21,7 @@
  *    getIntervalArray(3, 3) => [ 3 ]
  */
 function getIntervalArray(start, end) {
-  return Array.from([start], ...[end]);
+  return Array.from({ length: end - start + 1 }, (v, i) => i + start);
 }
 
 /**
